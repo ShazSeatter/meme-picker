86239e3fe267c249dbb8b630dc2af4531d7dece6
Shazreen Seatter <shazseatter12@gmail.com>
2023-10-17 20:10:02 +0100

    vite config file

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // ... other Vite configurations
+
+  // No need for specific image handling, Vite should handle it automatically
+});
