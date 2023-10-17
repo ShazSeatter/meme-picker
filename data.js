897992924615c60749aceea24345e08900fff97f
Shazreen Seatter <shazseatter12@gmail.com>
2023-10-17 14:45:41 +0100

    data constructed

---
+++ data.js
@@ -0,0 +1,123 @@
+export const natureData = [
+    {
+        themeTags: ["happy"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/8OJdqYqN1Nii3UTD6l/giphy.gif",
+        alt: "Famous artist Bob Ross holding a deer, experiencing the emotion of happiness",
+    },
+    {
+        themeTags: ["happy", "relaxed", "curious"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/5zunlU97tg6J4YqSj0/giphy.gif",
+        alt: "Underwater with a tiny octopus reaching to touch a divers finger",
+    },
+    {
+        themeTags: ["relaxed"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/Ck7tBhIVDs41duNLkY/giphy.gif",
+        alt: "Otter floating in the water, wet and yawning",
+    },
+    {
+        themeTags: ["relaxed", "beauty"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/33F5USzk3gJdZTinpS/giphy.gif",
+        alt: "Otter floating in the water, wet and yawning",
+
+    },
+    {
+        themeTags: ["funny"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/wGTu6DxVW6nLlYmdKk/giphy.gif",
+        alt: "Polar bear pushing its head along the ground",
+    },
+    {
+        themeTags: ["funny", "curious"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/d2Z99CRTQNnEYzmw/giphy.gif",
+        alt: "Ground hog popping out of hole",
+    },
+    {
+        themeTags: ["funny", "angry"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/l396O8kn1qbPcb4ha/giphy.gif",
+        alt: "Owl turning head around quickly and having a funny, angry face",
+    },
+    {
+        themeTags: ["curious"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/ilDSxue3KUq18kITTq/giphy.gif",
+        alt: "Man in a diving suit asking if he can touch something he is curious about",
+    },
+    {
+        themeTags: ["curious"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/DVJDjjK8m1y8fhQ151/giphy.gif",
+        alt: "Possem sniffing camera",
+    },
+    {
+        themeTags: ["beauty"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/Pheu6uCOwK27c99jL5/giphy.gif",
+        alt: "Rainforest, birds eye view with some mist surrounding the trees",
+    },
+    {
+        themeTags: ["beauty", "relaxing"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/WpxvQgNlk9q3SD6rXb/giphy.gif",
+        alt: "Female lion looking serene, and relaxed",
+    }, 
+    {
+        themeTags: ["beauty", "angry"],
+        isGif: true,
+        imageSrc: "https://media.giphy.com/media/c64m96xIm0ECc/giphy.gif",
+        alt: "Thunder and lighting",
+
+    }, 
+    {
+        themeTags: ["happy"],
+        isGif: false,
+        imageSrc: "happy-fox.jpg",
+        alt: "Fox in wilderness smiling and looking happy",
+    },
+    {
+        themeTags: ["happy", "relaxed", "beauty"],
+        isGif: false,
+        imageSrc: "happy-lion-cub.jpg",
+        alt: "Lion and its cub showing love",
+    }, 
+    {  
+        themeTags: ["funny", "curious"],
+        isGif: false,
+        imageSrc: "funny-curious-pug.jpg",
+        alt: "Pug wearing a denium jacket looking funny and curious with big eyes",
+    },
+    {
+        themeTags: ["curious"],
+        isGif: false,
+        imageSrc: "curious-seal.jpg",
+        alt: "Curious seal popping its head out of the water",
+    },
+    {
+        themeTags: ["relaxed", "beauty"],
+        isGif: false,
+        imageSrc: "relaxed-kola.jpg",
+        alt: "Kolabear relaxing and sleeping in a tree", 
+    }, 
+    {
+        themeTags: ["beauty"],
+        isGif: false,
+        imageSrc: "beauty-mountains.jpg",
+        alt: "Green, lushes mountains",
+    }, 
+    {
+        themeTags: ["angry"],
+        isGif: false,
+        imageSrc: "angry-storm.jpg",
+        alt: "Storm with dark clouds and lighting",
+
+    }
+]
+
+
+
+/* emotions: happy, relaxed, sad, empathetic, funny, surprised, angry, curious, beauty */
